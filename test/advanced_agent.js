e1bac729b1b46aeb263b58b22d4f452d3038e13c
Phillip Kovalev <kaero@yandex-team.ru>
2013-06-19 05:02:45 +0400

    add advanced_agent tests stub

---
+++ test/advanced_agent.js
@@ -0,0 +1,25 @@
+var http = require('http'),
+    Asker = require('../lib/asker'),
+    AdvancedAgent = require('../lib/advanced_agent');
+
+module.exports = {
+    'inheritance' : function(test) {
+
+        test.done();
+    },
+
+    'agents pool usage' : function(test) {
+
+        test.done();
+    },
+
+    'agent persistence' : function(test) {
+
+        test.done();
+    },
+
+    'removed socket event' : function(test) {
+
+        test.done();
+    }
+};
