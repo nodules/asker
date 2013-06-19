4bac3b5ac5bc98e223c1ff50f6d5f1fd834103dc
Phillip Kovalev <kaero@yandex-team.ru>
2013-06-19 21:51:19 +0400

    improoved Asker request ctor tests

--- test/constructor.js
+++ test/constructor.js
@@ -1,4 +1,6 @@
 var http = require('http'),
+    url = require('url'),
+    extend = require('extend'),
     Asker = require('../lib/asker'),
     AdvancedAgent = require('../lib/advanced_agent');
 
@@ -12,8 +14,8 @@ function testRequestWithoutOptions(request, test) {
             'option "' + option + '" value equal to default value');
     });
 
-    test.deepEqual(request.options.headers, {},
-        'empty headers by default');
+    test.deepEqual(request.options.headers, { 'accept-encoding' : 'gzip, *' },
+        'headers contains "accept-encoding" only by default');
 
     test.strictEqual(typeof request._onretry, 'undefined',
         'retry callback is undefined by default');
@@ -58,10 +60,48 @@ module.exports = {
         test.done();
     },
 
+    'common options' : function(test) {
+        var HOST = 'yandex.com',
+            PORT = 8080,
+            PATH = '/index.html',
+            HEADERS = {
+                'x-strange-header' : 'hello'
+            },
+            onretry = function() {},
+
+            request = new Asker({
+                host : HOST,
+                port : PORT,
+                path : PATH,
+                headers : HEADERS,
+                onretry : onretry
+            });
+
+        test.strictEqual(request.options.host, HOST,
+            'host option setted properly');
+
+        test.strictEqual(request.options.port, PORT,
+            'port option setted properly');
+
+        test.strictEqual(request.options.path, PATH,
+            'path option setted properly');
+
+        test.deepEqual(
+            request.options.headers,
+            extend(true, {}, HEADERS, { 'accept-encoding' : 'gzip, *' }),
+            'headers option setted properly');
+
+        test.strictEqual(request._onretry, onretry,
+            '"onretry" callback setted properly');
+
+        test.done();
+    },
+
     'url option' : function(test) {
         var HOST = 'yandex.com',
             PORT = 8080,
             PATH = '/index.html',
+            // @todo parsing without protocol #7
             URL = 'http://' + HOST + ':' + PORT + PATH,
 
             requestHPP = new Asker({
@@ -85,17 +125,95 @@ module.exports = {
         test.done();
     },
 
-    'common options' : function(test) {
+    'query and path args merging' : function(test) {
+        var PATH = '/test?hello=1',
+            PATH_2 = PATH + '&rainbow=bahamut',
+            QUERY = {
+                rainbow : 'unicorn',
+                world : '2'
+            },
+
+            requestMergedPath = new Asker({
+                path : PATH,
+                query : QUERY
+            }),
+
+            requestOverridenQuery = new Asker({
+                path : PATH_2,
+                query : QUERY
+            });
+
+        test.deepEqual(
+            url.parse(requestMergedPath.options.path, true).query,
+            extend(true, {}, url.parse(PATH, true).query, QUERY),
+            'path and query params merging without overriding');
+
+        test.deepEqual(
+            url.parse(requestOverridenQuery.options.path, true).query,
+            extend(true, {}, url.parse(PATH_2, true).query, QUERY),
+            'path and query params mreging WITH overriding existing path params');
 
         test.done();
     },
 
-    'GET specific query parsing' : function(test) {
+    // @todo issue #1
+    'POST request body building' : function(test) {
+        var QUERY_OBJECT = {
+                world : '2',
+                rainbow : 'unicorn'
+            },
+
+            QUERY_STRING = 'world=2&rainbow=unicorn',
+
+            requestQueryObject = new Asker({
+                method : 'POST',
+                body : QUERY_OBJECT
+            }),
+
+            requestQueryString = new Asker({
+                method : 'POST',
+                body : QUERY_STRING
+            });
+
+        test.strictEqual(requestQueryObject.options.body, JSON.stringify(QUERY_OBJECT),
+            'object stringified for request body');
+
+        test.strictEqual(requestQueryString.options.body, QUERY_STRING,
+            'set string body without modification');
+
+        test.strictEqual(
+            requestQueryString.options.headers['content-length'],
+            Buffer.byteLength(requestQueryString.options.body, 'utf8'),
+            'content-length header set to calculated value');
 
         test.done();
     },
 
-    'POST specific query parsing' : function(test) {
+    /*
+    // @todo failed now, issue #8
+    'lowercase methods names' : function(test) {
+        var request = new Asker({
+                method : 'post',
+                body : { test : 1 }
+            });
+
+        test.strictEqual(typeof request.options.body, 'string',
+            'request body processed as for POST, PUT, PATCH methods');
+
+        test.done();
+    },
+    */
+
+    'allow gzip option and header set' : function(test) {
+        var request = new Asker({
+                allowGzip : true,
+                headers : {
+                    'accept-encoding' : 'application/json'
+                }
+            });
+
+        test.strictEqual(request.options.headers['accept-encoding'], 'gzip, application/json',
+            'add "gzip" to list of accepted encodings if gzip support enabled');
 
         test.done();
     },
