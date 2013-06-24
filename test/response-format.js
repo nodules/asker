32ff203f743d792537f8af96b35e5ee9346eb48e
Alexey Rybakov <alexrybakov@yandex.ru>
2013-06-24 21:04:56 +0400

    more tests on response format; provide maxRetries in meta info, fixes #13

--- test/response-format.js
+++ test/response-format.js
@@ -13,16 +13,32 @@ module.exports = {
         this.server.close(callback);
     },
 
-    'check statusCode field' : function(test) {
+    'check response format' : function(test) {
+        var RESPONSE = 'response ok';
+
         this.server.addTest(function(req, res) {
             res.statusCode = 200;
-            res.end('response ok');
+            res.end(RESPONSE);
         });
 
-        test.expect(1);
+        test.expect(9);
 
         ask({ port : PORT }, function(error, response) {
+            test.strictEqual(error, null, 'no errors occured');
+
+            test.strictEqual(response.data, RESPONSE, 'response data is present and is a correct string');
+
             test.strictEqual(response.statusCode, 200, 'statusCode is present and equals 200');
+
+            test.strictEqual(typeof response.meta.time.network, 'number', 'network time is present and is a number');
+            test.strictEqual(typeof response.meta.time.total, 'number', 'total time is present and is a number');
+            test.ok(response.meta.time.total >= response.meta.time.network,
+                'total time should is greater or equals total time');
+
+            test.strictEqual(response.meta.retries.used, 0, 'no retries were used');
+            test.strictEqual(response.meta.retries.limit, 0, 'no retries allowed by default');
+
+            test.strictEqual(response.meta.options.port, PORT, 'port is correct');
 
             test.done();
         });
