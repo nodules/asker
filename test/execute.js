1f05f5ee858e3a6676ce4e9a6015dbb64eb51e0c
Phillip Kovalev <kaero@yandex-team.ru>
2013-07-03 22:42:02 +0400

    add test for Request#isRunning getter, fix #35

--- test/execute.js
+++ test/execute.js
@@ -48,5 +48,22 @@ module.exports = {
 
         assert.strictEqual(request.execute(), false,
             'second Request#execute call returns false');
-    })
+    }),
+
+    'Request#isRunning getter returns actual value of the private field _isRunning' : function(done) {
+        var request = new Asker();
+
+        assert.strictEqual(typeof request._isRunning, 'undefined',
+            'default _isRunning state is "unefined"');
+
+        assert.strictEqual(request.isRunning, false,
+            'value returned by isRunning getter equals private field value');
+
+        request._isRunning = true;
+
+        assert.strictEqual(request.isRunning, true,
+            'value returned by isRunning getter equals private field value');
+
+        done();
+    }
 };
