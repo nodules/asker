2825f36afbea1c503b103e733724745018a4909a
Phillip Kovalev <kaero@yandex-team.ru>
2013-07-04 04:28:44 +0400

    add httpRequest "error" event listener test

--- test/body.js
+++ test/body.js
@@ -205,6 +205,23 @@ module.exports = {
         });
     }),
 
+    'httpRequest error event listener test' : httpTest(function(done, server) {
+        server.addTest(function(req, res) {
+            res.writeHead(201, { 'ктулху\nотаке' : 333 });
+            res.end();
+        });
+
+        ask({ port : server.port }, function(error, response) {
+            assert.strictEqual(error.code, Asker.Error.CODES.HTTP_CLIENT_REQUEST_ERROR,
+                'http parser error recieved');
+
+            assert.strictEqual(typeof response, 'undefined',
+                'response is not compiled');
+
+            done();
+        });
+    }),
+
     // @todo needs to be fixed
     // if content-length recieved, then don't wait for more chunks
     'response body compilation with "content-length" more than actual content length' : httpTest(function(done, server) {
