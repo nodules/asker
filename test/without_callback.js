49ccca118bed9399c3f93dce091c3723f43bf98b
Phillip Kovalev <kaero@yandex-team.ru>
2013-07-04 18:00:12 +0400

    add test for request witout callback #54

---
+++ test/without_callback.js
@@ -0,0 +1,21 @@
+var Asker = require('../lib/asker'),
+    ask = Asker,
+    httpTest = require('./lib/http'),
+    assert = require('chai').assert;
+
+module.exports = {
+    'execute request without callback' : httpTest(function(done, server) {
+        var PATH = '/test';
+
+        server.addTest(function(req, res) {
+            assert.strictEqual(req.url, PATH,
+                'request recieved');
+
+            res.end();
+
+            done();
+        });
+
+        ask({ port : server.port, path : PATH });
+    })
+};
