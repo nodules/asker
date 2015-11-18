b7e5df4e7306b801c2e9ce6d0e45471058d9908b
Vladimir Varankin <nek.narqo+git@gmail.com>
2015-11-18 23:38:33 +0300

    add tests for different https cases
    closes #114

--- test/error_listener.js
+++ test/error_listener.js
@@ -1,10 +1,10 @@
-var Asker = require('../lib/asker'),
-    ask = Asker,
+var ask = require('../lib/asker'),
     httpTest = require('./lib/http'),
+    httpsTest = require('./lib/https'),
     assert = require('chai').assert;
 
 module.exports = {
-    'httpRequest error event listener test': httpTest(function(done, server) {
+    'http: error event listener test': httpTest(function(done, server) {
         server.addTest(function(req, res) {
             try {
                 res.writeHead(201, { 'ктулху\nотаке': 333 });
@@ -16,11 +16,26 @@ module.exports = {
         });
 
         ask({ port: server.port }, function(error, response) {
-            assert.strictEqual(error.code, Asker.Error.CODES.HTTP_CLIENT_REQUEST_ERROR,
-                'http parser error recieved');
+            assert.strictEqual(error.code, ask.Error.CODES.HTTP_CLIENT_REQUEST_ERROR,
+                'got http parser error');
 
-            assert.strictEqual(typeof response, 'undefined',
-                'response is not compiled');
+            assert.isUndefined(response, 'response was not compiled');
+
+            done();
+        });
+    }),
+
+    'https: error event listener test': httpsTest(function(done, server) {
+        server.addTest(function(req, res) {
+            res.end();
+        });
+
+        // @note: test server uses "self signed certificate"
+        ask({ protocol: server.protocol, port: server.port }, function(error, response) {
+            assert.strictEqual(error.code, ask.Error.CODES.HTTP_CLIENT_REQUEST_ERROR,
+                'got self signed certificate error');
+
+            assert.isUndefined(response, 'response was not compiled');
 
             done();
         });
---
+++ test/https.js
@@ -0,0 +1,121 @@
+var ask = require('../lib/asker'),
+    httpsTest = require('./lib/https'),
+    assert = require('chai').assert,
+    RESPONSE = 'ok done';
+
+module.exports = {
+    'https: self signed certificate': httpsTest(function(done, server) {
+        server.addTest(function(req, res) {
+            res.end(RESPONSE);
+        });
+
+        ask({ protocol: server.protocol, port: server.port }, function(error, response) {
+            assert.strictEqual(error.code, ask.Error.CODES.HTTP_CLIENT_REQUEST_ERROR,
+                'got self signed certificate error');
+            assert.isUndefined(response, 'response was not compiled');
+            done();
+        });
+    }),
+
+    'https: self signed certificate (custom agent)': httpsTest(function(done, server) {
+        server.addTest(function(req, res) {
+            res.end(RESPONSE);
+        });
+
+        var opts = {
+            protocol: server.protocol,
+            port: server.port,
+            agent: { name: 'agent1' }
+        };
+
+        ask(opts, function(error, response) {
+            assert.strictEqual(error.code, ask.Error.CODES.HTTP_CLIENT_REQUEST_ERROR,
+                'got self signed certificate error');
+            assert.isUndefined(response, 'response was not compiled');
+            done();
+        });
+    }),
+
+    'https: ignore self signed certificate': httpsTest(function(done, server) {
+        server.addTest(function(req, res) {
+            res.end(RESPONSE);
+        });
+
+        var opts = {
+            protocol: server.protocol,
+            port: server.port,
+            rejectUnauthorized: false
+        };
+
+        ask(opts, function(error, response) {
+            assert.isNull(error, 'self signed certificate was ignored');
+            assert.strictEqual(response.data.toString(), RESPONSE, 'response was compiled');
+            done();
+        });
+    }),
+
+    'https: ignore self signed certificate (custom agent)': httpsTest(function(done, server) {
+        server.addTest(function(req, res) {
+            res.end(RESPONSE);
+        });
+
+        var opts = {
+            protocol: server.protocol,
+            port: server.port,
+            agent: { name: 'agent2' },
+            rejectUnauthorized: false
+        };
+
+        ask(opts, function(error, response) {
+            assert.isNull(error, 'self signed certificate was ignored');
+            assert.strictEqual(response.data.toString(), RESPONSE, 'response was compiled');
+            done();
+        });
+    }),
+
+    'https: pass proper CA for self signed certificate': httpsTest(function(done, server) {
+        server.addTest(function(req, res) {
+            res.end(RESPONSE);
+        });
+
+        var opts = {
+            protocol: server.protocol,
+            port: server.port,
+            ca: server.rootCA
+        };
+
+        ask(opts, function(error, response) {
+            assert.isNull(error, 'self signed certificate was accepted');
+            assert.strictEqual(response.data.toString(), RESPONSE, 'response was compiled');
+            done();
+        });
+    }),
+
+    'https: pass proper CA for self signed certificate (custom agent)': httpsTest(function(done, server) {
+        server.addTest(function(req, res) {
+            res.end(RESPONSE);
+        });
+
+        var opts = {
+            protocol: server.protocol,
+            port: server.port,
+            agent: { name: 'agent3' },
+            ca: server.rootCA
+        };
+
+        ask(opts, function(error, response) {
+            assert.isNull(error, 'self signed certificate was accepted');
+            assert.strictEqual(response.data.toString(), RESPONSE, 'response was compiled');
+            done();
+        });
+    }),
+
+    'https: sanity check': function(done) {
+        ask({ url: 'https://www.yandex.com/', timeout: 3000 }, function(error, response) {
+            assert.isNull(error);
+            assert.strictEqual(response.statusCode, 200);
+            assert.ok(response.data.length > 0);
+            done();
+        });
+    }
+};
